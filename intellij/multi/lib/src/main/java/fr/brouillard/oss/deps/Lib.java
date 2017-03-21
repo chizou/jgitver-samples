36362ed033ec86148faeff2e206e7979cfb7c097
Matthieu Brouillard <matthieu@brouillard.fr>
2017-03-21 15:31:39 +0100

    adding a sample project to demo intellij features

---
+++ intellij/multi/lib/src/main/java/fr/brouillard/oss/deps/Lib.java
@@ -0,0 +1,7 @@
+package fr.brouillard.oss.deps;
+
+public class Lib {
+    public static String toUpperCase(String message) {
+        return message == null ? null : message.toUpperCase();
+    }
+}
---
+++ intellij/multi/mod/src/main/java/fr/brouillard/oss/deps/Mod.java
@@ -0,0 +1,7 @@
+package fr.brouillard.oss.deps;
+
+public class Mod {
+    public static void main( String[] args ) {
+        System.out.println(Lib.toUpperCase("Hello World!"));
+    }
+}
