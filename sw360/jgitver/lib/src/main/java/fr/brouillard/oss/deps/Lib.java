7729816fdb9c9543ec04b83bb03d261c917e62eb
Matthieu Brouillard <matthieu@brouillard.fr>
2017-03-21 14:07:39 +0100

    add sample for sw360 usecase
    see jgitver/jgitver-maven-plugin#54
    see sw360/sw360portal#358

---
+++ sw360/jgitver/lib/src/main/java/fr/brouillard/oss/deps/Lib.java
@@ -0,0 +1,9 @@
+package fr.brouillard.oss.deps;
+
+public class Lib
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sw360/jgitver/mod/src/main/java/fr/brouillard/oss/deps/Mod.java
@@ -0,0 +1,13 @@
+package fr.brouillard.oss.deps;
+
+/**
+ * Hello world!
+ *
+ */
+public class Mod
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
